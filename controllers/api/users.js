d0eabbe208ad37751c49510069a0440d7e85c3b9
EmmaZhang <emmazxd@gmail.com>
2024-03-18 18:22:10 +1100

    set user state

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,10 +1,32 @@
 /* eslint-disable no-undef */
-function create(req, res) {
-  const userData = {
-    name: req.body.name,
-    email: req.body.email,
-  };
-  res.json(userData);
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
+
+//for handling HTTP requests related to user creation in a RESTful API.
+async function create(req, res) {
+  try {
+    //The create() function is to handle the creation of a new user.
+    const user = await User.create(req.body);
+
+    const token = createJWT(user);
+
+    res.json(token);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+//helper function
+function createJWT(userData) {
+  //sign (in jsonwebtoken library) -> create JWTs
+  const syncToken = jwt.sign(
+    // data payload
+    { user: userData },
+    process.env.SECRET,
+    { expiresIn: "24h" }
+  );
+  console.log("syncToken", syncToken);
+  return syncToken;
 }
 
 module.exports = {
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -2,5 +2,30 @@ import * as usersAPI from "./users-api";
 
 export async function signUp(userData) {
   const token = await usersAPI.signUp(userData);
+  //save token in localstorage
+  localStorage.setItem("token", token);
   return token;
+}
+
+export function getToken() {
+  //distract token from local storage
+  const token = localStorage.getItem("token");
+
+  if (!token) return null;
+
+  //extract token from payload
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  // if token expire, remove it
+  if (payload.exp < Date.now() / 1000) {
+    localStorage.removeItem("token");
+    return null;
+  }
+
+  return token;
+}
+
+export function getUser() {
+  const token = getToken();
+  //.user refer to users.js payload define -> jwt.sign() key
+  return token ? JSON.parse(token.splict(".")[1].user) : null;
 }
