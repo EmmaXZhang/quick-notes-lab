4ddbaef9e4e22ebf9ef068874fc864f170ba2fb5
EmmaZhang <emmazxd@gmail.com>
2024-03-18 21:21:37 +1100

    add conditionally render form in AuthPage

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,11 +1,35 @@
 /* eslint-disable react/prop-types */
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LogInForm/LogInForm";
+import { useState } from "react";
 
 export default function AuthPage({ setUser }) {
+  const [showLogInForm, setShowLogInForm] = useState(false);
+
+  function handleSwitchForm() {
+    setShowLogInForm(!showLogInForm);
+  }
+
   return (
     <>
       <h1>AuthPage</h1>
-      <SignUpForm setUser={setUser} />
+      {showLogInForm ? (
+        <>
+          <LoginForm />
+          <p>
+            Don&apos;t have account ?
+            <button onClick={handleSwitchForm}>Switch to SignUp</button>
+          </p>
+        </>
+      ) : (
+        <>
+          <SignUpForm setUser={setUser} />
+          <p>
+            Already have an account{" "}
+            <button onClick={handleSwitchForm}>Swith to SignIn</button>
+          </p>
+        </>
+      )}
     </>
   );
 }
