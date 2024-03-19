e135a3836a05a2636e6f599432227ec47d0bad8f
EmmaZhang <emmazxd@gmail.com>
2024-03-19 15:54:15 +1100

    addNewNote complete on front end

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,9 +2,8 @@ import "./App.css";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { getUser } from "../../utilities/users-service";
-import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import NotesPage from "../NotesPage/NotesPage";
 import AuthPage from "../AuthPage/AuthPage";
-import OrderHistoryPage from "../OrderPageHistory/OrderPageHistory";
 import NavBar from "../../components/NavBar/NavBar";
 
 function App() {
@@ -16,8 +15,7 @@ function App() {
         <>
           <NavBar user={user} setUser={setUser} />
           <Routes>
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/notes" element={<NotesPage />} />
           </Routes>
         </>
       ) : (
--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -1,7 +1,41 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
 export default function NotesPage() {
+  const [newNote, setNewNote] = useState("");
+  const [noteList, setNoteList] = useState([]);
+
+  // function addToNoteList(noteToAdd) {
+  //   setNoteList([...noteList, noteToAdd]);
+  // }
+
+  function handleAddNote(event) {
+    event.preventDefault();
+    setNoteList([...noteList, newNote]);
+    setNewNote("");
+  }
+
   return (
     <>
       <h1>Quck Notes</h1>
+      <form onSubmit={handleAddNote}>
+        <textarea
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        ></textarea>
+        <button type="submit">Add Note</button>
+      </form>
+
+      <hr />
+      {noteList.length === 0 ? (
+        <p>No Notes Yet !</p>
+      ) : (
+        <ul>
+          {noteList.map((note, index) => (
+            <li key={index}>{note}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
