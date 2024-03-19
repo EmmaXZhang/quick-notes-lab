2ba1e611f82ce26394d85845164ca53f373ceafb
EmmaZhang <emmazxd@gmail.com>
2024-03-19 21:05:34 +1100

    display descending date order

--- src/components/NoteForm/NoteList.jsx
+++ src/components/NoteForm/NoteList.jsx
@@ -1,19 +1,30 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+
 export function NoteList({ noteList }) {
+  const [reverse, setReverse] = useState(false);
+
+  if (noteList.length === 0) {
+    <p>No Notes Yet!</p>;
+  }
+
+  const list = noteList.map((note, index) => (
+    <li key={index}>
+      <p>{note.text}</p>
+      <p>{new Date(note.createdAt).toLocaleString()}</p>
+    </li>
+  ));
+
   return (
     <div>
-      {noteList.length === 0 ? (
-        <p>No Notes Yet!</p>
-      ) : (
-        <ul>
-          {noteList.map((note, index) => (
-            <li key={index}>
-              <p>{note.text}</p>
-              <p>{note.createdAt}</p>
-            </li>
-          ))}
-        </ul>
-      )}
+      <button
+        onClick={() => {
+          setReverse(!reverse);
+        }}
+      >
+        Display descending date
+      </button>
+      {reverse ? list.reverse() : list}
     </div>
   );
 }
